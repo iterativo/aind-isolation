da5810164bad01b33e0dd8c9540ccead9d923f06
Rafael Torres <iterativo@users.noreply.github.com>
2017-04-27 08:24:52 -0500

    Add test to assert pruned branch was not traversed

--- agent_test.py
+++ agent_test.py
@@ -12,77 +12,77 @@ import sample_players
 from importlib import reload
 
 
-# class MinimaxPlayerTest(unittest.TestCase):
-#     """Unit tests for minimax player"""
+class MinimaxPlayerTest(unittest.TestCase):
+    """Unit tests for minimax player"""
 
-#     def setUp(self):
-#         reload(game_agent)
+    def setUp(self):
+        reload(game_agent)
 
-#     def test_best_score_when_last_layer_is_min_in_shallow_tree(self):
-#         # Arrange
-#         self.player1 = game_agent.MinimaxPlayer(
-#             score_fn=self.fake_score_fn, search_depth=1)
-#         self.player2 = sample_players.GreedyPlayer()
-#         self.game = isolation.Board(self.player1, self.player2, width=4, height=4)
-#         self.game.apply_move((0, 0)) # player 1
-#         self.game.apply_move((3, 3)) # player 2
+    def test_best_score_when_last_layer_is_min_in_shallow_tree(self):
+        # Arrange
+        self.player1 = game_agent.MinimaxPlayer(
+            score_fn=self.fake_score_fn, search_depth=1)
+        self.player2 = sample_players.GreedyPlayer()
+        self.game = isolation.Board(self.player1, self.player2, width=4, height=4)
+        self.game.apply_move((0, 0)) # player 1
+        self.game.apply_move((3, 3)) # player 2
 
-#         # Act
-#         best_move = self.player1.get_move(self.game, fake_time_left)
+        # Act
+        best_move = self.player1.get_move(self.game, fake_time_left)
 
-#         # Assert
-#         self.assertEqual(best_move, (1, 2))
+        # Assert
+        self.assertEqual(best_move, (1, 2))
 
-#     def test_best_score_when_last_layer_is_max_in_shallow_tree(self):
-#         # Arrange
-#         self.player1 = game_agent.MinimaxPlayer(
-#             score_fn=self.fake_score_fn, search_depth=2)
-#         self.player2 = sample_players.GreedyPlayer()
-#         self.game = isolation.Board(self.player1, self.player2, width=4, height=4)
-#         self.game.apply_move((0, 0)) # player 1
-#         self.game.apply_move((3, 3)) # player 2
+    def test_best_score_when_last_layer_is_max_in_shallow_tree(self):
+        # Arrange
+        self.player1 = game_agent.MinimaxPlayer(
+            score_fn=self.fake_score_fn, search_depth=2)
+        self.player2 = sample_players.GreedyPlayer()
+        self.game = isolation.Board(self.player1, self.player2, width=4, height=4)
+        self.game.apply_move((0, 0)) # player 1
+        self.game.apply_move((3, 3)) # player 2
 
-#         # Act
-#         best_move = self.player1.get_move(self.game, fake_time_left)
+        # Act
+        best_move = self.player1.get_move(self.game, fake_time_left)
 
-#         # Assert
-#         self.assertEqual(best_move, (2, 1))
+        # Assert
+        self.assertEqual(best_move, (2, 1))
 
-#     def test_best_score_when_last_layer_is_min_in_deep_tree(self):
-#         # Arrange
-#         self.player1 = game_agent.MinimaxPlayer(
-#             score_fn=self.fake_score_fn, search_depth=3)
-#         self.player2 = sample_players.GreedyPlayer()
-#         self.game = isolation.Board(self.player1, self.player2, width=5, height=5)
-#         self.game.apply_move((0, 0)) # player 1
-#         self.game.apply_move((4, 4)) # player 2
+    def test_best_score_when_last_layer_is_min_in_deep_tree(self):
+        # Arrange
+        self.player1 = game_agent.MinimaxPlayer(
+            score_fn=self.fake_score_fn, search_depth=3)
+        self.player2 = sample_players.GreedyPlayer()
+        self.game = isolation.Board(self.player1, self.player2, width=5, height=5)
+        self.game.apply_move((0, 0)) # player 1
+        self.game.apply_move((4, 4)) # player 2
 
-#         # Act
-#         best_move = self.player1.get_move(self.game, fake_time_left)
+        # Act
+        best_move = self.player1.get_move(self.game, fake_time_left)
 
-#         # Assert
-#         self.assertEqual(best_move, (1, 2))
+        # Assert
+        self.assertEqual(best_move, (1, 2))
 
-#     def fake_score_fn(self, game, player):
-#         """Stub score responses here"""
+    def fake_score_fn(self, game, player):
+        """Stub score responses here"""
 
-#         player1_location = game.get_player_location(player)
-#         player2_location = game.get_player_location(game.get_opponent(player))
-#         default_score = 2
-#         score = {
-#             ### (player1_location, player2_location): score ###
+        player1_location = game.get_player_location(player)
+        player2_location = game.get_player_location(game.get_opponent(player))
+        default_score = 2
+        score = {
+            ### (player1_location, player2_location): score ###
 
-#             # for test_best_score_when_last_layer_is_min_in_shallow_tree
-#             ((1, 2), (3, 3)): 3,
+            # for test_best_score_when_last_layer_is_min_in_shallow_tree
+            ((1, 2), (3, 3)): 3,
 
-#             # for test_best_score_when_last_layer_is_max_in_shallow_tree
-#             ((2, 1), (1, 2)): 3,
+            # for test_best_score_when_last_layer_is_max_in_shallow_tree
+            ((2, 1), (1, 2)): 3,
 
-#             # for test_best_score_when_last_layer_is_min_in_deep_tree
-#             ((2, 0), (3, 2)): 3,
-#             ((2, 4), (2, 3)): 3
-#         }
-#         return score.get((player1_location, player2_location), default_score)
+            # for test_best_score_when_last_layer_is_min_in_deep_tree
+            ((2, 0), (3, 2)): 3,
+            ((2, 4), (2, 3)): 3
+        }
+        return score.get((player1_location, player2_location), default_score)
 
 
 class AlphaBetaPlayerTest(unittest.TestCase):
@@ -99,7 +99,7 @@ class AlphaBetaPlayerTest(unittest.TestCase):
         self.game = isolation.Board(self.player1, self.player2, width=5, height=5)
         self.game.apply_move((0, 0)) # player 1
         self.game.apply_move((4, 4)) # player 2
-        self.score_counter = 10 # to coordinate with fake_score_fn
+        self.score_counter = 4 # to coordinate with fake_score_fn
         self.expected_best_move = None # updated by fake_score_fn
 
         # Act
@@ -107,6 +107,23 @@ class AlphaBetaPlayerTest(unittest.TestCase):
 
         # Assert
         self.assertEqual(self.expected_best_move, best_move)
+
+    def test_pruned_brach_is_not_traversed(self):
+        # Arrange
+        self.player1 = game_agent.AlphaBetaPlayer(
+            score_fn=self.fake_score_fn, search_depth=2)
+        self.player2 = sample_players.GreedyPlayer()
+        self.game = isolation.Board(self.player1, self.player2, width=5, height=5)
+        self.game.apply_move((0, 0)) # player 1
+        self.game.apply_move((4, 4)) # player 2
+        self.score_counter = 4 # to coordinate with fake_score_fn
+        self.expected_best_move = None # updated by fake_score_fn
+
+        # Act
+        self.player1.get_move(self.game, fake_time_left)
+
+        # Assert
+        self.assertEqual(1, self.score_counter, "Pruned branch was traversed")
 
     def fake_score_fn(self, game, player):
         """
@@ -121,7 +138,7 @@ class AlphaBetaPlayerTest(unittest.TestCase):
             # it stores player's 1 first move in the game
             self.expected_best_move = game.get_player_location(player)
 
-        self.score_counter -= self.score_counter
+        self.score_counter -= 1
         return self.score_counter
 
 
